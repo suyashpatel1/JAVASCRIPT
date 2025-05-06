491d3f7dc91bbdc5b1edc0c4827c7a34ee6f990b
suyashpatel1 <suyashpatel134@gmail.com>
2025-05-06 18:49:29 +0530

    js

---
+++ NEW.js
@@ -0,0 +1,104 @@
+
+function Assemble(){
+    // TASK1
+// var age = prompt("enter your age")
+// age = (age>=18)?"eligble" : "not eligible";
+// alert(age)
+// TASK2
+// var isCompleted=confirm("did you completed course and have certificate ")
+// a = isCompleted? "ACHIVEMENT SUCCEED": "PLEASE COMPLETE YOUR COURSE AND CERTIFICAITON";
+// alert(a)
+// TASK3
+// var price=prompt("What is the price of your item")
+// var a=confirm("DO you have coupon or not")
+
+// if(a){
+//     var Discount= confirm("10% Discount coupon applied")
+// }
+// else{
+//     var Discount= confirm("NO discount available")
+// }
+
+
+
+// alert(a)
+// Task4
+// var marks = prompt("Enter your marks")
+// if(marks>=33){
+//     alert("you are passed")
+// }
+// else{
+//     alert("You are failed")
+// }
+// TASK5
+// var a=prompt("Enter any number")
+// if (a%2==0){
+//     alert ("Even")
+// }
+// else{
+//     alert("Odd")
+// }
+// TASK6
+// var Delete=confirm("Are you sure to delete.")
+
+// if(Delete){
+//     var a=alert("Item deleted.")
+
+    
+// }
+// else{
+//     var a=alert("cancelled")
+// }TASK7
+
+// var number=prompt("Enter any number.")
+// if(number>=0){
+//     alert("Positive Number")
+// }
+// else{
+// alert("Negative Number")
+// }
+
+// TASK8
+// var i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// task9
+// var num;
+// do {
+//   num = prompt("Enter a number greater than 10:")
+// } while (num <= 10)
+//     console.log("stop")
+// task10
+var day = prompt("Enter a day (1 to 7)");
+
+switch(day) {
+  case "1":
+    console.log("1 for Monday");
+    break;
+  case "2": 
+    console.log("2 for Tuesday");
+    break;
+  case "3": 
+    console.log("3 for Wednesday");
+    break;
+  case "4": 
+    console.log("4 for Thursday");
+    break;
+  case "5": 
+    console.log("5 for Friday");
+    break;
+  case "6": 
+    console.log("6 for Saturday");
+    break;
+  case "7": 
+    console.log("7 for Sunday");
+    break;
+  default:
+    console.log("Something went wrong, please enter a number between 1 and 7");
+}
+
+
+}
+Assemble ()
