eccf009d60c0cfdef5f122418b54fe3595802008
suyashpatel1 <suyashpatel134@gmail.com>
2025-05-20 19:03:49 +0530

    update

---
+++ function.js
@@ -0,0 +1,22 @@
+// let arr =["siddhart",25,"Bhopal"]
+// let detail={
+//     name:"Siddhart",
+//     Age:"25",
+//     city:"Bhopal"
+// }
+// console.log(arr[2]);
+// console.log(detail.city)
+// detail.colege="MVM"
+// detail.age=26
+// console.log(detail)
+// arrow function
+// let welcome =(mmy name )=>{
+
+//     console.log("hello world")
+// }
+// welcome("Suyash")
+// let sum=(bats,balls)=>{
+// console.log("bats"+"balls")
+
+// }
+// sum(5,7)
